a0ba1d9e5ec132f3a0648f745c40aa4f793807c2
Nico Schlömer <nico.schloemer@gmail.com>
2020-04-29 13:50:14 +0200

    add log functionality, change grid color

--- cleanplotlib/__init__.py
+++ cleanplotlib/__init__.py
@@ -1,4 +1,4 @@
 from .__about__ import __version__
-from .main import multiplot, plot, savefig, show
+from .main import multiplot, plot, savefig, show, xlabel, ylabel
 
-__all__ = ["__version__", "multiplot", "plot", "show", "savefig"]
+__all__ = ["__version__", "multiplot", "plot", "show", "savefig", "xlabel", "ylabel"]
